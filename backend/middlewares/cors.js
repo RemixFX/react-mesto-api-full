50ab0de7f3015eb2d5e4b2dffe57a26c75eec48a
RemixFX <remix.master@yandex.ru>
2022-02-09 17:31:14 +0300

    fix: add Access-Control-Allow-Credential

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,6 +9,7 @@ module.exports = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+  res.header('Access-Control-Allow-Credentials', true);
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
